a8d7ecfd23574137bbbc8cba9630e350cf77f8d8
Jochem Brouwer <jochembrouwer96@gmail.com>
2018-11-29 00:23:30 +0100

    change hash logic

--- src/MerkleTree.js
+++ src/MerkleTree.js
@@ -12,6 +12,44 @@ class MerkleTree {
       hashes = [];
     }
 
+    if (hashList.length == 1){
+      output.hashRight[0] = false;
+      output.hashes[0] = hashList[0];
+      return output;
+    }
+
+    let tempList = list;
+    let Web3 = this.web3;
+    let currentInterest = interestedIn;
+
+    while (true) {
+      let Tree = [];
+      let key;
+      for (key = 0; key < tempList.length; key += 2){
+        let data = Web3.eth.abi.encodeParameters(['bytes32'], [tempList[key]]);
+        let hash;
+        if (tempList[key+1]) {
+          let data2 = Web3.eth.abi.encodeParameters(['bytes32'], [tempList[key+1]]);
+          let toHash = Web3.eth.abi.encodeParameters(['bytes32','bytes32'], [data, data2] )
+          hash = Web3.utils.soliditySha3(toHash);        
+        } else {
+          let toHash = Web3.eth.abi.encodeParameters(['bytes32','bytes32'], [data, data] )
+          hash = Web3.utils.soliditySha3(data);
+        }
+
+        Tree.push(hash);
+      }
+
+      tempList = Tree;
+  
+      if (Tree.length == 1){
+        break;
+      }
+    }
+
+
+
+    return output;
   }
 
   getHash(address, balance){
